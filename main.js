df4fd4147938897d40085439545d80182297ef81
Omar Faruk <70847377+omar-faruk01@users.noreply.github.com>
2021-05-03 18:26:19 -0400

    www.ofaruk.com
    HTML, CSS, and JS files for the portfolio site

---
+++ main.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    $(".menu-toggler").on('click', function () {
+        $(".menu-toggler").toggleClass('open');
+        $(".top-nav").toggleClass('open');
+    });
+
+    $(".top-nav .nav-link").on('click', function () {
+        $(".menu-toggler").removeClass('open');
+        $(".top-nav").removeClass('open');
+    });
+
+    $('nav a[href*="#"]').on('click', function () {
+        $('html, body').animate({
+            scrollTop: $($(".menu-toggler").attr("href")).offset().top - 100
+        });
+    });
+
+    $('#up').on('click', function () {
+        $('html, body').animate({
+            scrollTop: 0
+        });
+    });
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1800,
+        once: true
+    });
+
+});
+
